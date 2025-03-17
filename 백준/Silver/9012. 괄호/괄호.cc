f4a650a5d8f9d5625b214cd1b45dfa15abb10090
roi <109638033+intheWorldRoi@users.noreply.github.com>
2025-03-18 02:19:11 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+	int T;
+	string str;
+	cin >> T;
+	for (int i = 0; i < T; i++) // 테스트 케이스만큼 반복
+	{
+		stack<char> stack;
+		cin >> str;
+		for (int j = 0; j < str.size(); j++)
+		{
+			if (str[j] == '(')
+			{
+				stack.push(str[j]);
+			}
+			else if (str[j] == ')')
+			{
+				if (stack.empty()) // 괄호의 짝이 안 맞는 경우
+				{
+					stack.push('(');  break;
+				}
+				else
+				{
+					stack.pop();
+				}
+			}
+		}
+		if (stack.empty()) cout << "YES\n";
+		else cout << "NO\n";
+	}
+}
