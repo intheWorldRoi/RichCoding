2f78ed9aea89946257e4a22ea30c729ace306fbe
roi <109638033+intheWorldRoi@users.noreply.github.com>
2025-03-29 00:03:05 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string id;
+    cin >> id;
+    cout << ":fan::fan::fan:" << "\n";
+    cout << ":fan::" << id<< "::fan:" << "\n";
+    cout << ":fan::fan::fan:";
+}
