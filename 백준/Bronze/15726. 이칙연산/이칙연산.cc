5c895114613614fed2e350c1b6cb80acb39b76e5
roi <109638033+intheWorldRoi@users.noreply.github.com>
2025-04-09 00:16:19 +0900

    [Bronze IV] Title: 이칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15726. 이칙연산/이칙연산.cc
+++ 백준/Bronze/15726. 이칙연산/이칙연산.cc
@@ -7,8 +7,6 @@ int main() {
 
     long double A, B, C;
     cin >> A >> B >> C;
-
-    // 나눗셈을 먼저 수행하여 오버플로우 방지
     long long result1 = (long long)(A * B / C);
     long long result2 = (long long)(A / B * C);
 
