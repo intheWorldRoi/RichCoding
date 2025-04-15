214ff244b7c04bd0c169a4556deaa9f4bb08d095
roi <109638033+intheWorldRoi@users.noreply.github.com>
2025-04-16 01:55:03 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++) {
+        int off, on;
+        cin >> off >> on;
+    }
+
+    cout << "비와이" << '\n';
+    return 0;
+}
