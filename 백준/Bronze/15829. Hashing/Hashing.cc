06075c2d4f270f43503522c31d456a69ad6c0e24
roi <109638033+intheWorldRoi@users.noreply.github.com>
2026-03-12 00:01:40 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2208 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+
+const int M = 1234567891;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	int L;
+	string str;
+	cin >> L >> str;
+
+
+	int H{ 0 };
+	for (int i = 0; i < L; i++)
+	{
+		H += (alphabet.find(str[i])+1) * pow(31, i);
+	}
+
+	H %= M;
+
+	cout << H;
+}
